e9211c9b9d7d838bd7fc04766c482920040e32f4
Omar Beslic <omar.beslic@igamingplatform.com>
2025-02-21 20:13:00 +0100

    Fixed initial error and changed base api since its changed on be

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ import { store } from "./store/store";
 import App from "./App";
 
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
